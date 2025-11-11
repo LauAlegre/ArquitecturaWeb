414eaae82289910843928c611ac9992cbd16163c
JuliGonzlz <juliangonzalezjose018@gmail.com>
2025-11-11 20:55:26 -0300

    Realizo el metodo 4-e

---
+++ tpe-especial/microservicio-viajes/src/main/java/org/example/microservicioviajes/client/UsuarioClient.java
@@ -0,0 +1,18 @@
+package org.example.microservicioviajes.client;
+
+import org.example.microservicioviajes.dto.UsoDTO;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.time.LocalDate;
+
+@Component
+public class UsuarioClient {
+
+
+
+}
+
+
+
